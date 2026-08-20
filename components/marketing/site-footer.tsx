9099cd542ceba7bccf64e68d5807b50959e63c56
ShriyanDeyEngineer <shriyandey07@gmail.com>
2026-08-20 17:14:57 -0500

    Edit website footer

--- components/marketing/site-footer.tsx
+++ components/marketing/site-footer.tsx
@@ -52,11 +52,15 @@ export function SiteFooter() {
           &#8200;||&#8200;
           <Link href="/privacy_policy" className="text-white/80 hover:text-gold">Privacy Policy</Link>
         </p>
-
-        <p className="mt-2 text-center text-xs text-white/60">
-          ©2026 Goldy's Study Buddies. All rights reserved.
-        </p>
+        
       </div>
     </footer>
   );
 }
+
+/**
+ Paste the following on line 55 once appropriate to use
+ <p className="mt-2 text-center text-xs text-white/60">
+    ©2026 Goldy's Study Buddies. All rights reserved.
+  </p>
+ */
